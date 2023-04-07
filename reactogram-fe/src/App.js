72b131a1a67e6cf2735177175a420a87d4012fa0
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-07 15:34:55 +0530

    Design Sign up Page

--- reactogram-fe/src/App.js
+++ reactogram-fe/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
 
 function App() {
   return (
     <>
     
-    <Login />
+   {/* <Login />*/}
+   <Signup />
     </>
   );
 }
--- reactogram-fe/src/Pages/Signup/Signup.js
+++ reactogram-fe/src/Pages/Signup/Signup.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './Login.css'
 import SocialDesk from '../../Images/social-desktop.PNG';
 import SocialMob from '../../Images/social-mobile.PNG';
+import './Signup.css'
 
-const Login = () => {
-     return (
-          <div className="container login-container">
+const Signup = () => {
+  return (
+    <div className="container login-container">
                <div className="row">
                     <div className=" col-md-7 col-sm-12 d-flex justify-content-center align-items-center">
                          <img className='socialDesktop' style= {{height:'85%'}} src={SocialDesk} alt="social img" />
@@ -14,21 +14,23 @@ const Login = () => {
                     <div className="col-md-5 col-sm-12 ">
                          <div className="card shadow mb-5 bg-body rounded" >
                               <div className="card-body px-5">
-                                   <h4 className="card-title text-center fw-bold">Log In</h4>
+                                   <h4 className="card-title text-center fw-bold">Sign Up</h4>
                                    <form>
+                                   <input type="email" className="p-2 mb-2 mt-4 form-control input-bg" placeholder='Phone ' />
 
-                                        <input type="email" className="p-2 mb-2 mt-4 form-control input-bg" placeholder='Phone number,username or email' />
+                                        <input type="email" className="p-2 mb-2  form-control input-bg" placeholder='Email' />
+                                        <input type="text" className="p-2 mb-2  form-control input-bg" placeholder='Full name' />
                                         <input type="password" className="p-2 my-2 form-control input-bg" placeholder='Password' />
                                         <div className="mt-3 d-grid">
-                                             <button className="custom-btn custom-btn-blue">Log in</button>
+                                             <button className="custom-btn custom-btn-blue">Sign Up</button>
                                         </div>
                                         <div className='my-4'>
                                              <hr />
                                              <h5 className='text-center text-muted'>OR</h5>
                                              <hr />
                                         </div>
-                                        <div className=" mb-5 mt-3 d-grid">
-                                             <button className="custom-btn custom-btn-white"><span className='text-muted fs-6'>Dont't have an account?</span><span className='ms-1 text-info fw-bold'> Signup</span></button>
+                                        <div className="mt-3 mb-3 d-grid">
+                                             <button className="custom-btn custom-btn-white"><span className='text-muted fs-6'>Already have an account?</span><span className='ms-1 text-info fw-bold'> Login</span></button>
                                         </div>
 
 
@@ -41,7 +43,7 @@ const Login = () => {
                </div>
 
           </div>
-     )
+  )
 }
 
-export default Login;
+export default Signup;
