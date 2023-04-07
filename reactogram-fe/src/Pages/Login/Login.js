1e2c2b64fcf734cfe395ee08ea1fae4fabf76a5f
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-07 10:14:18 +0530

    Designed login page

--- reactogram-fe/src/Pages/Login/Login.js
+++ reactogram-fe/src/Pages/Login/Login.js
@@ -18,8 +18,17 @@ const Login = () => {
                                              <input  type="email" className="p-2 mb-2 mt-4 form-control input-bg" placeholder='Phone number,username or email'   />
                                              <input type="password" className="p-2 my-2 form-control input-bg" placeholder='Password'/>
                                              <div className="mt-3 d-grid">
-                                                <button type="submit" className="custom-btn custom-btn-blue">Log in</button>
+                                                <button  className="custom-btn custom-btn-blue">Log in</button>
                                              </div>
+                                             <div className='my-4'>
+                                             <hr />
+                                             <h5 className='text-center text-muted'>OR</h5>
+                                             <hr />
+                                             </div>
+                                             <div className="mt-3 d-grid">
+                                                <button  className="custom-btn custom-btn-white"><span className='text-muted fs-6'>Dont't have an account?</span><span className='ms-1 text-info fw-bold'> Signup</span></button>
+                                             </div>
+
                                              
                                         
                                    </form>
