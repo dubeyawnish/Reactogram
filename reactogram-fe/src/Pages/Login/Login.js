a30dcc971755488dbb6671d3c0eddd0d687359c3
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-07 09:12:30 +0530

    reactogram img added

--- reactogram-fe/src/Pages/Login/Login.js
+++ reactogram-fe/src/Pages/Login/Login.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import './Login.css'
+import SocialDesk from '../../Images/social-desktop.PNG';
 
 const Login = () => {
-  return (
-    <div className="container login-container">
-       <div className="row">
-            <div className=" col-md-7 border border-primary">
-                 image section
-            </div>
-            <div className="col-md-5 border border-danger">
-                 form section
-            </div>
-       </div>
-      
-    </div>
-  )
+     return (
+          <div className="container login-container">
+               <div className="row">
+                    <div className=" col-md-7 ">
+                         <img src={SocialDesk} alt="social img" />
+                    </div>
+                    <div className="col-md-5 ">
+                         <div className="card shadow p-3 mb-5 bg-body rounded" >
+                              <div className="card-body px-5">
+                                   <h4 className="card-title text-center fw-bold">Log In</h4>
+                                   <form>
+                                        
+                                             <input  type="email" className="p-2 mb-2 mt-4 form-control input-bg" placeholder='Phone number,username or email'   />
+                                             <input type="password" className="p-2 my-2 form-control input-bg" placeholder='Password'/>
+                                             <button type="submit" className="btn btn-primary">Submit</button>
+                                        
+                                   </form>
+
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
+          </div>
+     )
 }
 
 export default Login;
