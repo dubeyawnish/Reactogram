2c3f6e34627f027995bdd2c12d006df1999528ed
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-12 19:49:44 +0530

    popup post show

--- reactogram-fe/src/Pages/Profile/Profile.js
+++ reactogram-fe/src/Pages/Profile/Profile.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Profile.css'
 import { Button, Modal } from 'react-bootstrap'
 import { API_BASE_URL } from '../../config';
 import axios from 'axios'
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const navigate = useNavigate();
 
+  const user = useSelector(state => state.userReducer);
 
   const [image, setImage] = useState({ preview: '', data: '' });
 
@@ -18,6 +20,9 @@ const Profile = () => {
 
   const [caption, setCaption] = useState('');
   const [location, setLocation] = useState('');
+  const [myallposts, setMyallposts] = useState([]);
+
+  const [showDetail,setShowDetail]= useState({});
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -100,6 +105,32 @@ const Profile = () => {
 
   }
 
+  useEffect(() => {
+    getMyallpost();
+  }, [])
+
+
+
+  const getMyallpost = async () => {
+    const response = await axios.get(`${API_BASE_URL}/myallposts`, CONFIG_OBJ);
+    if (response.status === 200) {
+      setMyallposts(response.data.author);
+     // debugger;
+    }
+    else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Some error occured while getting all your  post'
+      })
+    }
+
+  }
+ 
+
+  const showPostDetails= (post)=>{
+     setShowDetail(post);
+
+  }
 
 
 
@@ -110,10 +141,10 @@ const Profile = () => {
 
           <div className='d-flex flex-column '>
             <img className='img-fluid profil-pic p-2' src='https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='Profile pic' />
-            <p className='ms-3 fs-5 fw-bold'>john_doe_ful4</p>
-            <p className='ms-3 fs-5'>john doe </p>
-            <p className='ms-3 fs-5'>UI/UX Designer @udonixo follow @rhsul </p>
-            <p className='ms-3 fs-5'>My portfolio on www.portfolio.com/jhon </p>
+            <p className='ms-3 fs-5 fw-bold'>{user.user.email}</p>
+            <p className='ms-3 fs-5'>{user.user.fullName} </p>
+            <p className='ms-3 fs-5'>UI/UX Designer @{user.user.fullName} follow @rhsul </p>
+            <p className='ms-3 fs-5'>My portfolio on www.portfolio.com/{user.user.fullName} </p>
 
           </div>
 
@@ -121,7 +152,7 @@ const Profile = () => {
         <div className='col-md-6 d-flex flex-column  justify-content-between'>
           <div className='d-flex justify-content-equal'>
             <div className='count-section pe-md-5 pe-4 text-center fw-bold' >
-              <h6> 10</h6>
+              <h6> {myallposts.length}</h6>
               <p> Post</p>
             </div>
             <div className='count-section text-center fw-bold ps-md-5 pe-md-5 ps-4 pe-4'>
@@ -154,73 +185,32 @@ const Profile = () => {
       </div>
 
       <div className='row mb-4'>
-        <div className='col-md-4 col-sm-12 mb-md-0  mb-2'>
-          <div className="card" >
-            <img onClick={handleShow} src="https://plus.unsplash.com/premium_photo-1677178628425-84a64dc416b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className=" card-img-top" alt="..." />
 
-          </div>
-        </div>
+        {
+          myallposts.map((post) => {
+            return (
+              <div className='col-md-4 col-sm-12 mb-md-0  mb-2'>
+                <div className="card "style={{width:'23rem',height:'15rem'}} >
+                  <img onClick={()=>{showPostDetails(post);
+                  handleShow();}}  src={post.image} className=" card-img-top" alt="post" />
 
-        <div className='col-md-4 col-sm-12 mb-md-0 mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1582794543139-8ac9cb0f7b11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className=" card-img-top" alt="..." />
+                </div>
+              </div>
+            )
+          })
 
-          </div>
-        </div>
+        }
 
-        <div className='col-md-4 col-sm-12 mb-md-0 mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1469259943454-aa100abba749?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className="img-fluid card-img-top" alt="..." />
 
-          </div>
-        </div>
+
+
+
       </div>
 
-      <div className='row mb-4'>
-        <div className='col-md-4 col-sm-12 mb-md-0  mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1508610048659-a06b669e3321?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZsb3dlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className=" card-img-top" alt="..." />
 
-          </div>
-        </div>
 
-        <div className='col-md-4 col-sm-12 mb-md-0 mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1468327768560-75b778cbb551?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZsb3dlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className=" card-img-top" alt="..." />
 
-          </div>
-        </div>
 
-        <div className='col-md-4 col-sm-12 mb-md-0 mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1476994230281-1448088947db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGZsb3dlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className="card-img-top" alt="..." />
-
-          </div>
-        </div>
-      </div>
-
-      <div className='row mb-4'>
-        <div className='col-md-4 col-sm-12 mb-md-0  mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGZsb3dlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className=" card-img-top" alt="..." />
-
-          </div>
-        </div>
-
-        <div className='col-md-4 col-sm-12 mb-md-0 mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1455659817273-f96807779a8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGZsb3dlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className=" card-img-top" alt="..." />
-
-          </div>
-        </div>
-
-        <div className='col-md-4 col-sm-12 mb-md-0 mb-2'>
-          <div className="card" >
-            <img src="https://images.unsplash.com/photo-1488928741225-2aaf732c96cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGZsb3dlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className="card-img-top" alt="..." />
-
-          </div>
-        </div>
-      </div>
 
 
       <Modal show={show} onHide={handleClose} size='lg'>
@@ -252,13 +242,13 @@ const Profile = () => {
                   </div>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3ByaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100" alt="..." />
+                      <img src={showDetail.image} className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                      <img src="https://images.unsplash.com/photo-1470240731273-7821a6eeb6bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c3ByaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100" alt="..." />
+                      <img src={showDetail.image} className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                      <img src="https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNwcmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className="d-block w-100" alt="..." />
+                      <img src={showDetail.image} className="d-block w-100" alt="..." />
                     </div>
                   </div>
                   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -281,8 +271,8 @@ const Profile = () => {
                 <div className='col-6 d-flex'>
                   <img className=' p-2 post-profile-pic ' src='https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt="profile pic" />
                   <div>
-                    <p className='pt-2 fs-6 fw-bold'>Title</p>
-                    <p className='location'>Description</p>
+                    <p className='pt-2 fs-6 fw-bold'>{showDetail.location}</p>
+                    <p className='location'>{showDetail.description}</p>
                   </div>
                 </div>
 
